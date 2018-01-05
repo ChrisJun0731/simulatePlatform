2997246ac82a0c16f91be353e79fce142ca7fedd
Administrator <1193030047@qq.com>
2018-01-05 11:30:52 +0800

    Initial commit

---
+++ src/main/java/com/genture/simulatePlatform/SimulatePlatformApplication.java
@@ -0,0 +1,14 @@
+package com.genture.simulatePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018/1/5.
+ */
+@SpringBootApplication
+public class SimulatePlatformApplication {
+	public static void main(String[] args){
+		SpringApplication.run(SimulatePlatformApplication.class);
+	}
+}
---
+++ src/main/java/com/genture/simulatePlatform/controller/DataController.java
@@ -0,0 +1,28 @@
+package com.genture.simulatePlatform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2018/1/5.
+ */
+@RestController
+public class DataController {
+	@RequestMapping("/gate")
+	public String response(HttpServletRequest req, HttpServletResponse resp){
+		String sid = req.getParameter("sid");
+		//result用于存储返回结果
+		String result = "";
+		if(sid.equals("30012")){
+			//获取道路状态
+		}
+		if(sid.equals("30011")){
+			//获取道路长度及旅行时间
+
+		}
+		return result;
+	}
+}
